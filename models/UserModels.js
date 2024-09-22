36e47c92f756205aff6a21dd09d85894678c7b65
iqbalrsyd <iqbalhidayatrasyad@mail.ugm.ac.id>
2024-09-22 22:17:26 +0700

    fixing logic auth

--- models/UserModels.js
+++ models/UserModels.js
@@ -20,71 +20,52 @@ const userSchema = new mongoose.Schema({
     minlength: [8, "Password should be at least 8 characters long"],
     select: false,
   },
-  confirmPassword: {
-    type: String,
-    validate: {
-      validator: function (el) {
-        return el === this.password;
-      },
-      message: "Passwords are not the same",
-    },
-  },
   googleId: { type: String, default: null },
-  twoFactorSecret: { type: String, default: null },
   passwordResetToken: { type: String, default: null },
   passwordResetExpires: { type: Date, default: null },
-  isTwoFactorEnabled: { type: Boolean, default: false },
   loginAttempts: {
     type: Number,
     default: 0,
   },
   lockoutTime: Date, 
-
   isVerified: {
     type: Boolean,
     default: false, 
   }, 
   emailVerificationToken: String,
-}); 
+});
 
+// Middleware untuk hashing password sebelum menyimpan
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 12);
-  this.confirmPassword = undefined; 
   next();
 });
 
-userSchema.methods.comparePasswordToDB = async function(password, passwordDB) {
-  return await bcrypt.compare(password, passwordDB);
-}
-
-userSchema.methods.isPasswordChanged = function(JWTTimestamp) {
-  if (this.passwordChangedAt) {
-    console.log(this.passwordChangedAt, JWTTimestamp);
-  }
-  return false;
+// Method untuk membandingkan password
+userSchema.methods.comparePasswordToDB = async function(password) {
+  return await bcrypt.compare(password, this.password);
 }
 
 userSchema.methods.createResetPasswordToken = function() {
   const resetToken = crypto.randomBytes(32).toString('hex');
-
   this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
-  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; 
-
-  console.log(resetToken, this.passwordResetToken);
-
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; // Token berlaku selama 10 menit
   return resetToken;
 }
 
-userSchema.methods.isLocked = function () {
+userSchema.methods.isLocked = function() {
   return this.lockoutTime && this.lockoutTime > Date.now();
 };
 
-userSchema.methods.incrementLoginAttempts = function () {
+
+userSchema.methods.incrementLoginAttempts = async function() {
   this.loginAttempts += 1;
+  await this.save({ validateBeforeSave: false }); // Ensure changes are saved
 };
 
-userSchema.methods.resetLoginAttempts = function () {
+
+userSchema.methods.resetLoginAttempts = function() {
   this.loginAttempts = 0;
   this.lockoutTime = undefined;
 };
